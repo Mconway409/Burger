34e0341f35e22f81e7f61aa56d063eb46be62bb1
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2021-03-19 16:08:45 -0400

    updates

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 8080;
 
@@ -19,7 +19,6 @@ const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
- 
+app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
 });
