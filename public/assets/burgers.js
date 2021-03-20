5d451b379e8554d254f71c04cfa58d48d0cf6aec
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2021-03-20 13:21:54 -0400

    updates

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,64 +1,48 @@
-// $(function () {
-//   $(".change-devour").on("click", function (event) {
-//     event.preventDefault();
-//     const id = $(this).data("id");
-//     const newDevour = $(this).data("newdevour");
-
-//     const newDevouredState = {
-//       devoured: newDevour,
-//     };
-//     $.ajax(`/api/burgers/${id}`, {
-//       type: "PUT",
-//       data: newDevouredState,
-//     }).then(function () {
-//       console.log("changed devoured to", newDevour);
-//       location.reload();
-//     });
-//   });
-
-//   $(".create-form").on("submit", function (event) {
-//     event.preventDefault();
-
-//     const burgerValue = $("#br").val().trim();
-//     if (burgerValue) {
-//       const newBurger = {
-//         burger_name: $("#br").val().trim(),
-//         devoured: 0,
-//       };
-//       $.ajax("/api/burgers", {
-//         type: "POST",
-//         data: newBurger,
-//       }).then(function () {
-//         console.log("created new burger");
-//         location.reload();
-//       });
-//     }
-//   });
-//   $(".delete-burger").on("click", function (event) {
-//     const id = $(this).data("id");
-//     $.ajax(`/api/burgers/${id}`, {
-//       type: "DELETE",
-//     }).then(function () {
-//       console.log("deleted burger", id);
-//       location.reload();
-//     });
-//   });
-// });
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
+$(function () {
+  $(".change-devour").on("click", function (event) {
     event.preventDefault();
+    const id = $(this).data("id");
+    const newDevour = $(this).data("newdevour");
 
-    var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id);
-    $.ajax({
-      method: "PUT",
-      url: "/burgers/" + burger_id
-    }).then(function(data) {
-      // reload page to display devoured burger in proper column
+    const newDevouredState = {
+      devoured: newDevour,
+    };
+    $.ajax(`/api/burgers/${id}`, {
+      type: "PUT",
+      data: newDevouredState,
+    }).then(function () {
+      console.log("changed devoured to", newDevour);
       location.reload();
     });
+  });
 
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    const burgerValue = $("#br").val().trim();
+    if (burgerValue) {
+      const newBurger = {
+        burger_name: $("#br").val().trim(),
+        devoured: 0,
+      };
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger,
+      }).then(function () {
+        console.log("created new burger");
+        location.reload();
+      });
+    }
+  });
+  $(".delete-burger").on("click", function (event) {
+    const id = $(this).data("id");
+    $.ajax(`/api/burgers/${id}`, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted burger", id);
+      location.reload();
+    });
   });
 });
+
 
